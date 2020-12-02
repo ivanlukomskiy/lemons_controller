3d495e65786c9b843ec033f893cd3421d9cba857
ivanl <ivanlukomskiy@gmail.com>
2020-12-03 01:17:35 +0300

    Use ADS1256

--- sensors.py
+++ sensors.py
@@ -1,17 +1,19 @@
-import gpioexp
+from ADS1256 import ADS1256
 
-from constants import LIGHT_SENSOR_PIN, RESISTIVE_GROUND_HUMIDITY_PIN, CAPACITIVE_GROUND_HUMIDITY_PIN
+from constants import LIGHT_SENSOR_CHANNEL, RESISTIVE_GROUND_HUMIDITY_CHANNEL, CAPACITIVE_GROUND_HUMIDITY_CHANNEL
 
-exp = gpioexp.gpioexp()
+ADC = ADS1256.ADS1256()
+ADC.ADS1256_init()
+ADC_Value = ADC.ADS1256_GetAll()
 
 
 def get_light_level():
-    return 1 / exp.analogRead(LIGHT_SENSOR_PIN)
+    return 1 / ADC.ADS1256_GetChannalValue(LIGHT_SENSOR_CHANNEL)
 
 
 def get_resistive_ground_humidity():
-    return exp.analogRead(RESISTIVE_GROUND_HUMIDITY_PIN)
+    return ADC.ADS1256_GetChannalValue(RESISTIVE_GROUND_HUMIDITY_CHANNEL)
 
 
 def get_capacitive_ground_humidity():
-    return exp.analogRead(CAPACITIVE_GROUND_HUMIDITY_PIN)
+    return ADC.ADS1256_GetChannalValue(CAPACITIVE_GROUND_HUMIDITY_CHANNEL)
